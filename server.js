3839bfb694cbf4b57884ca405b13d6c123236bc6
Andy <andyandyvu@gmail.com>
2026-09-07 13:57:08 +0200

    tilføjelse af post og formdata ejs osv

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 3333;
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (request, response) => {
+  response.render("index", { question: "" });
+});
+
+app.post("/ask", (request, response) => {
+  const question = request.body.question;
+  response.render("index", { question });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
