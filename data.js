25891831310e52bbad1f6c3aa61bb049ab2b6a52
Andy <andyandyvu@gmail.com>
2026-09-09 11:51:10 +0200

    data til regex

---
+++ data.js
@@ -0,0 +1,33 @@
+Mohanad Aboud
+Anas Amin Abu-Auon
+Emil Riisager Axelsen
+Tinus Behr
+Dan Okkels Brendstrup
+Martin Bysted
+Rasmus Vase Cederdorff
+Nynne Lucca Christiansen
+Athena Elena Habibi
+Andreas Hansen
+Albert Lund Hetting
+Anker Sand Kjær Knudsen
+Rune Frisch Knudsen
+Christian Kragh
+Sarah Boddum Kristiansen
+Malte Krog
+Annika Hecto Laursen
+Lasse Calvillo Nørregaard Laursen
+Stine Blaabjerg
+Mia Michelle Mogensen
+Ellen Magna Schwartz Moselund
+Bertil Hoffmann Møller Nielsen
+Frederik Nielsen
+Magnus Lindholm Nielsen
+Frederik Lenz Pedersen
+Mathilde Kirstine Rosted Poulsen
+Candela Richard
+Annette Marie Skou
+Sanne Ellegaard Therkildsen
+Jacob Aagaard Trier
+Julie Valbjørn
+Andy An Dinh Vu
+Huy Thanh Vu
